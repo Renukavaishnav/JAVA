6feecda835f7b03a8c972514209c2bd8efeb3258
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 14:15:16 +0530

    check even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd {
+     public static void main(String[] args) {
+         
+        //Check Even or Odd Number:
+
+    int a =24;
+
+    if(a%2==0){
+        System.out.print("Even number");
+    }else{
+        System.out.print("Even number");
+
+    }
+     }
+    
+}
