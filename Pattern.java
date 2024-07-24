c831d9112836d10f93e61f5a38ebef8342688d4c
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 14:36:33 +0530

    Oattern in java

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+public class Pattern {
+    public static void main(String [] args){
+
+        //Print Pattern in Java:
+        for (int i = 0; i < 10; i++) {
+             for(int j = 0; j < i; j++){
+
+                System.out.print("*");
+             }
+             System.out.print("\n");
+
+        }
+
+    }
+    
+}
