5fd8fca98ca7f2dff6c146767355158ee9a4706d
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 19:04:37 +0530

    Check Leap Year in Jav

---
+++ leapyear.java
@@ -0,0 +1,13 @@
+public class leapyear {
+    public static void main(String[] args) {
+        
+        int year = 2016;
+
+        if(year%4==0 || year%400==0 || year%100==0){
+            System.out.println(year + " is leap year" );
+        }else{
+            System.out.println(year + " is not leap year" );
+
+        }
+    }
+}
