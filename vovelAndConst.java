96f265068a1720c86ecd79282c3637f05240c257
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 19:17:40 +0530

    owel or Consonant in Java:

---
+++ vovelAndConst.java
@@ -0,0 +1,14 @@
+public class vovelAndConst {
+    public static void main(String[] args) {
+         char val = 'c';
+        
+        //Check Vowel or Consonant in Java:
+         if(val=='a'||val=='e'||val=='i'||val=='o'||val=='u'){
+            System.out.println(val+ " is a vovel");
+         }else{
+            System.out.println(val+ " is a Consonant");
+
+         }
+
+    }
+}
