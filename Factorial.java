114df82b9a05bc888b5772fd9d4a05e5850869fc
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 14:28:57 +0530

    Factorial of a Number:

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args){
+
+    //Factorial of a Number:
+    int num = 1;
+
+    for(int i=5; i>0; i--){
+    num=num*i;
+
+    }
+    System.out.println("Factorial is " + num );
+
+    }
+}
