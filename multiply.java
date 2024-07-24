6ad9640d071c1b76ce7f6fc6769f46ad07691350
renukaVaishnav <renuk7621@gmail.com>
2024-07-24 15:25:38 +0530

    Multiply Two Numbers in Java:

---
+++ multiply.java
@@ -0,0 +1,12 @@
+public class multiply {
+   
+    public static void main(String[] args) {
+        
+        int a = 27;
+        int b = 4;
+        int result = a*b;
+        //Addition of Two Numbers:
+        System.out.println(result);
+    }
+    
+}
