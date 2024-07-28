738f1e87bf3186c770a5c56ad7d9bb841b03f8aa
renukaVaishnav <renuk7621@gmail.com>
2024-07-28 16:05:01 +0530

    quotientAndRemainder

---
+++ quotientAndRemainder.java
@@ -0,0 +1,11 @@
+public class quotientAndRemainder{
+    public static void main(String [] args){
+
+        int numerator = 27, denominator = 5 ;
+        
+        int quotient = numerator/denominator;
+        int remainder = numerator%denominator;
+
+        System.out.println(quotient + " " + remainder);
+    }
+}
