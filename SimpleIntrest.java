8ad177b622b509af4606e8d96299d1ccb2e0eb69
renukaVaishnav <renuk7621@gmail.com>
2024-07-28 15:58:29 +0530

    SimpleIntrest

---
+++ SimpleIntrest.java
@@ -0,0 +1,11 @@
+public class SimpleIntrest {
+    public static void main(String[] args){
+        //Smple INterest
+        // Principal , Rate , Time
+        double principal = 100000, rate =5, time = 3;
+
+        Double SI = principal*rate*time/100;
+
+        System.out.println(SI);
+    }
+}
