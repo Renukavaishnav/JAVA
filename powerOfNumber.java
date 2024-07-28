a1c441d821be2a8b01bceca396b35679bc864e6e
renukaVaishnav <renuk7621@gmail.com>
2024-07-28 16:14:47 +0530

    powerOfNumber

---
+++ powerOfNumber.java
@@ -0,0 +1,14 @@
+public class powerOfNumber {
+    public static void main(String [] args){
+        
+        int power = 5;
+        int value = 2;
+
+        for(int i = 1; i<=power; i++){
+
+            System.out.println(value);
+             value*=2;
+        }
+    }
+    
+}
